1e52d43e7bd3badebbdb8cf4f2e1d4b568e42e43
Austin <austin.a.guevara@gmail.com>
2025-04-28 13:58:33 -0700

    copied over part1 and updated comments and tests to match part 2

--- AOC2015/day-02/src/bin/part2.rs
+++ AOC2015/day-02/src/bin/part2.rs
@@ -5,8 +5,39 @@ fn main() {
     dbg!(output);
 }
 
+// calculate the amount of ribbon necessary for each present
+// ribbon = min_perimeter + lwh
 pub fn part2(input: &str) -> i32 {
-    todo!();
+    let mut wrapping_paper_ft = 0;
+
+    for lines in input.lines() {
+        let mut box_deminsions: Vec<&str> = lines.split('x').collect();
+        let length: u32 = box_deminsions
+            .pop()
+            .expect("No value found")
+            .parse()
+            .expect("could not parse");
+        let width: u32 = box_deminsions
+            .pop()
+            .expect("No value found")
+            .parse()
+            .expect("could not parse");
+        let height: u32 = box_deminsions
+            .pop()
+            .expect("No value found")
+            .parse()
+            .expect("could not parse");
+
+        // calculate surface area
+        let side1 = length * width;
+        let side2 = width * height;
+        let side3 = height * length;
+        wrapping_paper_ft += 2 * (side1 + side2 + side3);
+        // calulate extra
+        wrapping_paper_ft += side1.min(side2).min(side3);
+    }
+
+    wrapping_paper_ft
 }
 
 #[cfg(test)]
@@ -14,11 +45,11 @@ mod tests {
     use super::*;
 
     #[test]
-    fn check2() {
-        let result = part2("");
-        assert_eq!(result, 0);
+    fn ribbon_len() {
+        let result = part2("2x3x4");
+        assert_eq!(result, 34);
 
-        let result = part2("");
-        assert_eq!(result, 0);
+        let result = part2("1x1x10");
+        assert_eq!(result, 14);
     }
 }
