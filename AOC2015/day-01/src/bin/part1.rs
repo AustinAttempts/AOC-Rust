d08397f8cfc0cf7f3a52502601711427931ebcae
Austin <austin.a.guevara@gmail.com>
2025-04-28 12:35:18 -0700

    added tests for part 1

--- AOC2015/day-01/src/bin/part1.rs
+++ AOC2015/day-01/src/bin/part1.rs
@@ -1,10 +1,14 @@
+// Advent of Code 2015 - Day 01: Not Quite Lisp
 fn main() {
     let input = include_str!("./input1.txt");
     let output = part1(input);
     dbg!(output);
 }
 
-pub fn part1(_input: &str) -> String {
+// Starting from 0 every '(' is +1 floor and
+// every ')' is -1 floor.
+// returns the final value
+pub fn part1(_input: &str) -> i32 {
     todo!()
 }
 
@@ -13,8 +17,41 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = part1("");
-        assert_eq!(result, "");
+    fn floor_0() {
+        let result = part1("(())");
+        assert_eq!(result, 0);
+
+        let result = part1("()()");
+        assert_eq!(result, 0);
+    }
+
+    #[test]
+    fn floor_3() {
+        let result = part1("(((");
+        assert_eq!(result, 3);
+
+        let result = part1("(()(()(");
+        assert_eq!(result, 3);
+
+        let result = part1("))(((((");
+        assert_eq!(result, 3);
+    }
+
+    #[test]
+    fn basement_1() {
+        let result = part1("())");
+        assert_eq!(result, -1);
+
+        let result = part1("))(");
+        assert_eq!(result, -1);
+    }
+
+    #[test]
+    fn basement_3() {
+        let result = part1(")))");
+        assert_eq!(result, -3);
+
+        let result = part1(")())())");
+        assert_eq!(result, -3);
     }
 }
---
+++ AOC2015/day-01/target/debug/deps/part2-1e8e56047bbec9cf.d
@@ -0,0 +1,5 @@
+/Users/austin/Developer/AOC-Rust/AOC2015/day-01/target/debug/deps/part2-1e8e56047bbec9cf: src/bin/part2.rs
+
+/Users/austin/Developer/AOC-Rust/AOC2015/day-01/target/debug/deps/part2-1e8e56047bbec9cf.d: src/bin/part2.rs
+
+src/bin/part2.rs:
