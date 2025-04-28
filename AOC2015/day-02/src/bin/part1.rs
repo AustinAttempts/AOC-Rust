bb7a72cb614330f4360d4cd29ef5d35522d6d3bb
Austin <austin.a.guevara@gmail.com>
2025-04-28 13:53:18 -0700

    solution to part 1

--- AOC2015/day-02/src/bin/part1.rs
+++ AOC2015/day-02/src/bin/part1.rs
@@ -6,9 +6,38 @@ fn main() {
 }
 
 // calculate the total amount of wrapping paper needed:
-// per present = 2(lw + wh + hl) + min(lwh)
+// per present = 2(lw + wh + hl) + min(side)
 pub fn part1(input: &str) -> u32 {
-    todo!();
+    let mut wrapping_paper_ft = 0;
+
+    for lines in input.lines() {
+        let mut box_deminsions: Vec<&str> = lines.split('x').collect();
+        let length: u32 = box_deminsions
+            .pop()
+            .expect("No value found")
+            .parse()
+            .expect("could not parse");
+        let width: u32 = box_deminsions
+            .pop()
+            .expect("No value found")
+            .parse()
+            .expect("could not parse");
+        let height: u32 = box_deminsions
+            .pop()
+            .expect("No value found")
+            .parse()
+            .expect("could not parse");
+
+        // calculate surface area
+        let side1 = length * width;
+        let side2 = width * height;
+        let side3 = height * length;
+        wrapping_paper_ft += 2 * (side1 + side2 + side3);
+        // calulate extra
+        wrapping_paper_ft += side1.min(side2).min(side3);
+    }
+
+    wrapping_paper_ft
 }
 
 #[cfg(test)]
--- AOC2015/day-02/target/debug/deps/part1-d080ea5319d19f8b.d
+++ AOC2015/day-02/target/debug/deps/part1-d080ea5319d19f8b.d
@@ -1,6 +1,6 @@
-/Users/austin/Developer/AOC-Rust/AOC2015/day-01/target/debug/deps/libpart1-d080ea5319d19f8b.rmeta: src/bin/part1.rs src/bin/./input1.txt
+/Users/austin/Developer/AOC-Rust/AOC2015/day-02/target/debug/deps/libpart1-d080ea5319d19f8b.rmeta: src/bin/part1.rs src/bin/./input1.txt
 
-/Users/austin/Developer/AOC-Rust/AOC2015/day-01/target/debug/deps/part1-d080ea5319d19f8b.d: src/bin/part1.rs src/bin/./input1.txt
+/Users/austin/Developer/AOC-Rust/AOC2015/day-02/target/debug/deps/part1-d080ea5319d19f8b.d: src/bin/part1.rs src/bin/./input1.txt
 
 src/bin/part1.rs:
 src/bin/./input1.txt:
