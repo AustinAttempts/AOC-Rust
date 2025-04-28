63a9531d19be9faf1df5dd39565c4e4126c9fb2a
Austin <austin.a.guevara@gmail.com>
2025-04-28 12:57:57 -0700

    updated tests and comments for part 2

--- AOC2015/day-01/src/bin/part2.rs
+++ AOC2015/day-01/src/bin/part2.rs
@@ -7,7 +7,11 @@ fn main() {
 
 // Starting from 0 every '(' is +1 floor and
 // every ')' is -1 floor.
-// returns the final value
+//
+// returns the index of instruction that first put santa
+// into the basement.
+//
+// Note: indexing is base 1 not 0!
 pub fn part1(input: &str) -> i32 {
     let mut floor = 0;
     for elem in input.chars() {
@@ -25,41 +29,11 @@ mod tests {
     use super::*;
 
     #[test]
-    fn floor_0() {
-        let result = part1("(())");
-        assert_eq!(result, 0);
+    fn enter_basement() {
+        let result = part1(")");
+        assert_eq!(result, 1);
 
-        let result = part1("()()");
-        assert_eq!(result, 0);
-    }
-
-    #[test]
-    fn floor_3() {
-        let result = part1("(((");
-        assert_eq!(result, 3);
-
-        let result = part1("(()(()(");
-        assert_eq!(result, 3);
-
-        let result = part1("))(((((");
-        assert_eq!(result, 3);
-    }
-
-    #[test]
-    fn basement_1() {
-        let result = part1("())");
-        assert_eq!(result, -1);
-
-        let result = part1("))(");
-        assert_eq!(result, -1);
-    }
-
-    #[test]
-    fn basement_3() {
-        let result = part1(")))");
-        assert_eq!(result, -3);
-
-        let result = part1(")())())");
-        assert_eq!(result, -3);
+        let result = part1("()())");
+        assert_eq!(result, 5);
     }
 }
