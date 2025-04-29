0b9a90ba95315982fc3f2a3719e5efd172ee14c3
Austin <austin.a.guevara@gmail.com>
2025-04-29 11:57:37 -0700

    solution to part 2

--- AOC2015/day-04/src/bin/part2.rs
+++ AOC2015/day-04/src/bin/part2.rs
@@ -1,3 +1,5 @@
+use md5::Digest;
+
 // Advent of Code 2015 - Day 04: The Ideal Stocking Stuffer
 fn main() {
     let input = include_str!("./input1.txt");
@@ -5,8 +7,25 @@ fn main() {
     dbg!(output);
 }
 
-fn part2(_input: &str) -> u32 {
-    todo!();
+fn part2(input: &str) -> u32 {
+    let mut counter: u32 = 0;
+    let mut hash = compute_hash(input, counter);
+    while !good_hash(hash) {
+        counter += 1;
+        hash = compute_hash(input, counter);
+    }
+
+    counter
+}
+
+fn good_hash(hash: Digest) -> bool {
+    // first 5 nibbles (4 bits) are 0
+    hash[0] == 0 && hash[1] == 0 && hash[2] == 0
+}
+
+fn compute_hash(input: &str, counter: u32) -> Digest {
+    let value = input.to_owned() + &counter.to_string();
+    md5::compute(value.as_bytes())
 }
 
 #[cfg(test)]
@@ -14,8 +33,14 @@ mod tests {
     use super::*;
 
     #[test]
-    fn repeated_houses() {
-        let result = part2("");
-        assert_eq!(result, 3);
+    fn secret_key_low() {
+        let result = part2("abcdef");
+        assert_ne!(result, 609043);
+    }
+
+    #[test]
+    fn secret_key_high() {
+        let result = part2("pqrstuv");
+        assert_ne!(result, 1048970);
     }
 }
---
+++ AOC2015/day-04/target/debug/deps/part2-e6560282c02b5988.d
@@ -0,0 +1,6 @@
+/Users/austin/Developer/AOC-Rust/AOC2015/day-04/target/debug/deps/part2-e6560282c02b5988: src/bin/part2.rs src/bin/./input1.txt
+
+/Users/austin/Developer/AOC-Rust/AOC2015/day-04/target/debug/deps/part2-e6560282c02b5988.d: src/bin/part2.rs src/bin/./input1.txt
+
+src/bin/part2.rs:
+src/bin/./input1.txt:
