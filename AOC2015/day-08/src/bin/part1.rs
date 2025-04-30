ee81f4f1122119809b2c5cd53f3992a4b40f22f6
Austin <austin.a.guevara@gmail.com>
2025-04-30 12:26:45 -0700

    boiler plate

---
+++ AOC2015/day-08/src/bin/part1.rs
@@ -0,0 +1,21 @@
+// Advent of Code 2015 - Day 08: Matchsticks
+fn main() {
+    let input = include_str!("./input1.txt");
+    let output = part1(input);
+    dbg!(output);
+}
+
+fn part1(_input: &str) -> i32 {
+    todo!();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn place_holder_test() {
+        let result = part1("");
+        assert_eq!(result, 0);
+    }
+}
---
+++ AOC2015/day-08/src/bin/part2.rs
@@ -0,0 +1,21 @@
+// Advent of Code 2015 - Day 08: Matchsticks
+fn main() {
+    let input = include_str!("./input1.txt");
+    let output = part2(input);
+    dbg!(output);
+}
+
+fn part2(_input: &str) -> i32 {
+    todo!();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn place_holder_test() {
+        let result = part2("");
+        assert_eq!(result, 0);
+    }
+}
