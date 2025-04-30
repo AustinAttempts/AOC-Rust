744681cc864a501efb26b2f5722624091cd5698f
Austin <austin.a.guevara@gmail.com>
2025-04-30 16:16:39 -0700

    solution to part 2

--- AOC2015/day-08/src/bin/part1.rs
+++ AOC2015/day-08/src/bin/part1.rs
@@ -5,8 +5,68 @@ fn main() {
     dbg!(output);
 }
 
-fn part1(_input: &str) -> i32 {
-    todo!();
+fn part1(input: &str) -> usize {
+    let mut string_len = 0;
+    let mut memory_len = 0;
+
+    for lines in input.lines() {
+        string_len += string_literals_len(lines);
+        memory_len += string_memory_len(lines);
+    }
+
+    string_len - memory_len
+}
+
+fn string_literals_len(str: &str) -> usize {
+    str.len()
+}
+
+fn string_memory_len(s: &str) -> usize {
+    let chars: Vec<char> = s.chars().collect();
+    let mut i = 1; // Skip opening quote
+    let mut mem_len = 0;
+
+    while i < chars.len() - 1 {
+        if chars[i] == '\\' {
+            if i + 1 >= chars.len() - 1 {
+                // Trailing backslash
+                i += 1;
+                mem_len += 1;
+            } else {
+                match chars[i + 1] {
+                    '\\' | '"' => {
+                        mem_len += 1;
+                        i += 2;
+                    }
+                    'x' => {
+                        if i + 3 < chars.len() {
+                            let hex_str = format!("{}{}", chars[i + 2], chars[i + 3]);
+                            if u8::from_str_radix(&hex_str, 16).is_ok() {
+                                mem_len += 1;
+                                i += 4;
+                            } else {
+                                // malformed escape
+                                mem_len += 1;
+                                i += 1;
+                            }
+                        } else {
+                            mem_len += 1;
+                            i += 1;
+                        }
+                    }
+                    _ => {
+                        mem_len += 1;
+                        i += 1;
+                    }
+                }
+            }
+        } else {
+            mem_len += 1;
+            i += 1;
+        }
+    }
+
+    mem_len
 }
 
 #[cfg(test)]
@@ -14,8 +74,62 @@ mod tests {
     use super::*;
 
     #[test]
-    fn place_holder_test() {
-        let result = part1("");
+    fn string_len_test() {
+        let result = string_literals_len("\"\"");
+        assert_eq!(result, 2);
+
+        let result = string_literals_len("\"abc\"");
+        assert_eq!(result, 5);
+
+        let result = string_literals_len("\"aaa\\\"aaa\"");
+        assert_eq!(result, 10);
+
+        let result = string_literals_len("\"\\x27\"");
+        assert_eq!(result, 6);
+    }
+
+    #[test]
+    fn memory_len_test() {
+        let result = string_memory_len("\"\"");
         assert_eq!(result, 0);
+
+        let result = string_memory_len("\"abc\"");
+        assert_eq!(result, 3);
+
+        let result = string_memory_len("\"aaa\\\"aaa\"");
+        assert_eq!(result, 7);
+
+        let result = string_memory_len("\"\\x27\"");
+        assert_eq!(result, 1);
+    }
+
+    #[test]
+    fn compare_test() {
+        let result = part1(
+            "\"abc\"
+        \"abc\"
+        \"aaa\\\"aaa\"
+        \"\\x27\"",
+        );
+        assert_eq!(result, 12);
+    }
+
+    #[test]
+    fn problem_child() {
+        let result = string_memory_len("\"\\\\x27\"");
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn invalid_hex_test() {
+        let result = string_memory_len("\"\\xZZ\"");
+        // This should probably return 4: \ x Z Z
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn backslash_at_end() {
+        let result = string_memory_len("\"abc\\\\\"");
+        assert_eq!(result, 4); // Should be: a b c \ 
     }
 }
--- AOC2015/day-08/src/bin/part2.rs
+++ AOC2015/day-08/src/bin/part2.rs
@@ -5,8 +5,25 @@ fn main() {
     dbg!(output);
 }
 
-fn part2(_input: &str) -> i32 {
-    todo!();
+fn part2(input: &str) -> usize {
+    let mut original_string_len = 0;
+    let mut new_string_len = 0;
+
+    for lines in input.lines() {
+        original_string_len += string_literals_len(lines);
+        new_string_len += string_encode(lines);
+    }
+
+    new_string_len - original_string_len
+}
+
+fn string_literals_len(str: &str) -> usize {
+    str.len()
+}
+
+fn string_encode(line: &str) -> usize {
+    let result = format!("{:?}", line);
+    result.len()
 }
 
 #[cfg(test)]
@@ -14,8 +31,43 @@ mod tests {
     use super::*;
 
     #[test]
-    fn place_holder_test() {
-        let result = part2("");
-        assert_eq!(result, 0);
+    fn string_len_test() {
+        let result = string_literals_len("\"\"");
+        assert_eq!(result, 2);
+
+        let result = string_literals_len("\"abc\"");
+        assert_eq!(result, 5);
+
+        let result = string_literals_len("\"aaa\\\"aaa\"");
+        assert_eq!(result, 10);
+
+        let result = string_literals_len("\"\\x27\"");
+        assert_eq!(result, 6);
+    }
+
+    #[test]
+    fn memory_len_test() {
+        let result = string_encode("\"\"");
+        assert_eq!(result, 6);
+
+        let result = string_encode("\"abc\"");
+        assert_eq!(result, 9);
+
+        let result = string_encode("\"aaa\\\"aaa\"");
+        assert_eq!(result, 16);
+
+        let result = string_encode("\"\\x27\"");
+        assert_eq!(result, 11);
+    }
+
+    #[test]
+    fn compare_test() {
+        let result = part2(
+            "\"abc\"
+        \"abc\"
+        \"aaa\\\"aaa\"
+        \"\\x27\"",
+        );
+        assert_eq!(result, 19);
     }
 }
