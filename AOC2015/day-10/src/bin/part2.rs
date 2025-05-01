3aacdf39563eee62207ecabf048f6e36e3ae5a73
Austin <austin.a.guevara@gmail.com>
2025-05-01 12:33:33 -0700

    day 10 boiler plate

---
+++ AOC2015/day-10/src/bin/part2.rs
@@ -0,0 +1,21 @@
+// Advent of Code 2015 - Day 09: All in a Single Night
+fn main() {
+    let input = include_str!("./input1.txt");
+    let output = part2(input);
+    dbg!(output);
+}
+
+fn part2(_input: &str) -> String {
+    todo!();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn place_holder() {
+        let result = part2("");
+        assert_eq!(result, "");
+    }
+}
