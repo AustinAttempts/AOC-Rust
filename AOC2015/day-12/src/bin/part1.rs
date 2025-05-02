e631174c25eca88b473dea5a59f421ca9801a025
Austin <austin.a.guevara@gmail.com>
2025-05-02 10:49:29 -0700

    solution to part1

--- AOC2015/day-12/src/bin/part1.rs
+++ AOC2015/day-12/src/bin/part1.rs
@@ -6,8 +6,12 @@ fn main() {
     dbg!(answer);
 }
 
-fn part1(_input: &str) -> i32 {
-    todo!();
+fn part1(input: &str) -> i32 {
+    let cleaned = input.replace(['[', ']', '{', '}', ':', ',', '"'], " ");
+    cleaned
+        .split_whitespace()
+        .filter_map(|token| token.parse::<i32>().ok())
+        .sum()
 }
 
 #[cfg(test)]
@@ -15,8 +19,29 @@ mod tests {
     use super::*;
 
     #[test]
-    fn place_holder() {
-        let result = part1("");
+    fn simple_add() {
+        let result = part1("[1,2,3]");
+        assert_eq!(result, 6);
+
+        let result = part1("{\"a\":2,\"b\":4}");
+        assert_eq!(result, 6);
+
+        let result = part1("[[[3]]]");
+        assert_eq!(result, 3);
+
+        let result = part1("{\"a\":{\"b\":4},\"c\":-1}");
+        assert_eq!(result, 3);
+
+        let result = part1("{\"a\":[-1,1]}");
+        assert_eq!(result, 0);
+
+        let result = part1("[-1,{\"a\":1}]");
+        assert_eq!(result, 0);
+
+        let result = part1("[]");
+        assert_eq!(result, 0);
+
+        let result = part1("{}");
         assert_eq!(result, 0);
     }
 }
